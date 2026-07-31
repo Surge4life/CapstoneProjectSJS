8440f827588605a6a4100576d045c56f806011d1
Surge4life <Sashin.singh@outlook.com>
2026-07-31 07:11:26 +0200

    Boot Client package mode metadata

--- udoc-app/src/main.tsx
+++ udoc-app/src/main.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { UDOC_PACKAGE } from "./packageMode";
 import "./styles.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><App/></React.StrictMode>);
+
+document.documentElement.dataset.udocPackage = UDOC_PACKAGE;
+if (typeof document !== "undefined" && document.title && !/Client/i.test(document.title)) {
+  document.title = "UDOC Client · Tenant SaaS · G.O.D.S";
+}
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
